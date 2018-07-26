d018a42ae7de152c772ccc7e9531c3b1f064b864
Simon Guertin <sim.guertin@gmail.com>
2018-07-26 16:33:27 -0400

    testing initialize method + require

--- lib/softledger.rb
+++ lib/softledger.rb
@@ -1,7 +1,35 @@
 class Softledger
+  require 'http'
+
+  attr_reader: access_token
+
+  def initialize(params)
+    @username = params[:username]
+    @password = params[:password]
+    @access_token = authenticate
+
+
+  end
+
   def self.hi(language)
     translator = Translator.new(language)
     translator.hi
+  end
+
+  private
+
+  def authenticate
+    # url = "https://softledger.auth0.com/oauth/token"
+
+    #   {
+    #     "grant_type": "string",
+    #     "tenantUUID": "string",
+    #     "audience": "string",
+    #     "client_id": "string",
+    #     "client_secret": "string"
+    #   }
+
+    return "Hello World of : #{@username} "
   end
 end
 
